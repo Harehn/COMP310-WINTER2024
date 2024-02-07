ba2cc3a5143e2c62c5f7a286b22d5385853c02b3
Nitin <harehn.kaundun@mail.mcgill.ca>
2024-02-07 16:03:32 -0500

    Minor issue with the help command again

--- a1/interpreter.c
+++ a1/interpreter.c
@@ -133,7 +133,7 @@ my_cat STRING		Opens the file STRING and prints the content\n ";
 	char help_string[] = "COMMAND			DESCRIPTION\n \
 help			Displays all the commands\n \
 quit			Exits / terminates the shell with “Bye!”\n \
-set VAR STRING		Assigns a value to shell memory with up to 5 tokens\n \
+set VAR STRING		Assigns a value to shell memory\n \
 print VAR		Displays the STRING assigned to VAR\n \
 run SCRIPT.TXT		Executes the file SCRIPT.TXT\n ";
 	printf("%s\n", help_string);
