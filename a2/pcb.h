5ae00696f895f57556d32d1eaefdf801624093d3
Danlin Luo <danlinluo432@gmail.com>
2024-03-01 21:24:26 -0500

    Add comments to pcb.h

--- a2/pcb.h
+++ a2/pcb.h
@@ -7,11 +7,11 @@
  * priority: priority flag
  * pid: process(task) id
  * PC: program counter, stores the index of line that the task is executing
- * page_count: 
- * offset_end:
+ * page_count: the number of total pages in the pcb
+ * offset_end: the offset in the last page, used to avoid reading extra lines
  * page_table: array to hold the indices of the pages
- * offset:s
- * valid_page:
+ * offset: the current offset, stores the which line the program is reading in the current page
+ * valid_page: a table storing true if the page is in memory, false otherwise
  * job_length_score: for EXEC AGING use only, stores the job length score
  * filename: name of the file from which commands are read from
  * fp: Filestream object to read from
